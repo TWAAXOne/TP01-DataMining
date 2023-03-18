9273816d9b602d6e5d2f6866125385cab8328204
jeff <jeff.goncalves@etu.hesge.ch>
2023-03-18 14:33:01 +0100

    commit chelou sur la branche du j

--- Jeff.R
+++ Jeff.R
@@ -2,5 +2,13 @@ dataSet <- read.table(file = "data/HR_prediction-train.csv", header = TRUE)
 # number of instances
 n <- nrow(dataSet)
 #
+dim(dataSet)
+# quel est la variable cible ?
+# la variable cible est la variable "left"
+#montre moi les valeurs manquantes
+table(is.na(dataSet))
+# il n'y a pas de valeurs manquantes
+#Pour chaque attributs qualitatif f , calcule la distribution de probabilité de la variable cible
+
 
 
